7e58ab1b9f81440653bd30c8367ddf06eb6f9984
Goyamjain05 <128897307+Goyamjain05@users.noreply.github.com>
2024-07-10 12:53:45 +0000

    saved

--- 0_1basics/datatypes.js
+++ 0_1basics/datatypes.js
@@ -1,1 +1,14 @@
 //alert("hello")-->not working qas we are using node js and not browser
+//alert(3+3)
+let name="Goyam"//string
+const age=13//number 2 to the power 53
+let loggedIn=false//boolean 
+//null -> Standalone value
+let state=null
+console.log(state)
+//Symbol --> for uniqueness
+console.log(typeof(name))
+console.log(typeof(age))
+console.table([typeof(name),typeof(age)])
+console.log(typeof(null))////object
+console.log(typeof undefined)//undefined

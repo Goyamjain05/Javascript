760edbf7b223f7c07dc8391be323037d405f8b29
Goyamjain05 <128897307+Goyamjain05@users.noreply.github.com>
2024-07-10 13:33:41 +0000

    conversion of datatype

---
+++ 0_1basics/conversion.js
@@ -0,0 +1,14 @@
+// let score="33abc"
+// console.log(typeof score)
+// let valueinNumber=Number(score)
+// console.log(typeof(valueinNumber))
+// console.log(valueinNumber)
+//"33" =>33
+//"33abc"==>Nan not a number
+let isLoggedIn=1
+console.log(typeof isLoggedIn)
+let boolval=Boolean(isLoggedIn)
+console.log(typeof boolval)
+console.log(boolval)
+//1=>true 0=>false
+//""=> false "value"=>true
